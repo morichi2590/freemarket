9e53af1600ea374e80486c788fe4b4a5fd59f2a4
ChiakiMori <s.zero.s2590@gmail.com>
2019-01-17 13:46:50 +0900

    EDIT deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 # config valid for current version and patch releases of Capistrano
 
-#ここからコメントアウト
 lock "~> 3.11.0"
 
 set :application, 'freemarket'
@@ -37,7 +36,6 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
-#ここまでコメントアウト
 
 
 # Default branch is :master
@@ -74,11 +72,9 @@ end
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-
-#コメントアウト
-# set :default_env, {
-#   rbenv_root: "/usr/local/rbenv",
-#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-#   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-# }
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
